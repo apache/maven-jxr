6f50a1c74d0d3aabe1f0223c5b3791f713ab7276
Matt Nelson <matt.nelson@cerner.com>
2019-01-18 14:58:03 -0600

    Add non-forking jxr/test-jxr goals

---
+++ maven-jxr-plugin/src/main/java/org/apache/maven/plugin/jxr/JxrNoForkReport.java
@@ -0,0 +1,39 @@
+package org.apache.maven.plugin.jxr;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * Creates an html-based, cross referenced version of Java source code
+ * for a project with forking. Note that this goal does require generation of sources before
+ * site generation, e.g. by invoking <tt>mvn clean deploy site</tt>.
+ *
+ * @since 3.1.0
+ * @version $Id$
+ */
+@Mojo( name = "jxr-no-fork" )
+@Execute( phase = LifecyclePhase.NONE )
+public class JxrNoForkReport
+    extends JxrReport
+{
+}
